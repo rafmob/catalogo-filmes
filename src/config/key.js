27051b638e8fc69409deaec63572c9a4bf5e0994
rafmob <89050871+rafmob@users.noreply.github.com>
2022-06-18 16:49:06 -0300

    Finalizacao

---
+++ src/config/key.js
@@ -0,0 +1,1 @@
+export const APIKey = '616444757d8c40a2e2e6f426cac97f64';
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+    padding: 4rem 0;
+
+    h1 {
+        margin: 3rem 0;
+    }
+
+    .movie {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 100%;
+        max-width: 300px;
+        border-radius: 1rem;
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 3rem;
+        max-width: 50%;
+    }
+
+    button {
+        background: #6654DA;
+        border-color: none;
+        cursor: pointer;
+        border-radius: 1rem;
+        color: #fff;
+        padding: .8rem 2rem;
+        margin-top: 1rem;
+        font-size: 100%
+        transition all 0.3s
+    }
+
+    button:hover {
+        background: #5848C2;
+    }
+
+    span {
+        line-height: 130%;
+        margin-bottom: 1rem;
+        font-size: 110%;
+
+    }
+
+    .release-date {
+        opacity: 0.5;
+
+    }
+
+` 
